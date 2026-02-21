04a54a5090289a8ca5ec698c179c6817a1c9f905
SuryatejRaju <86778157+SuryatejRaju@users.noreply.github.com>
2026-02-21 17:09:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,25 @@
-resource "google_container_cluster" "primary" {
-  name     = "hackathon-gke-cluster"
-  location = var.zone
+resource "google_container_cluster" "gke" {
+  name     = var.cluster_name
+  location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  cluster    = google_container_cluster.primary.name
-  location   = var.zone
+  name       = "node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.gke.name
 
   node_config {
     machine_type = "e2-medium"
   }
 
-  node_count = 1
+  initial_node_count = 1
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  location      = var.region
+  repository_id = "hackathon-repo"
+  format        = "DOCKER"
 }
