cd96063bdb127558a87a38f87fb633d47c05897d
SuryatejRaju <86778157+SuryatejRaju@users.noreply.github.com>
2026-02-21 17:10:28 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "cluster_name" {
-  value = google_container_cluster.primary.name
+  value = google_container_cluster.gke.name
+}
+
+output "artifact_repo" {
+  value = google_artifact_registry_repository.repo.repository_id
 }
